84157455f82c4677ed0ff9532e90880e381755f3
Sergey Kadushkin <blackoverlord.inc@mail.ru>
2023-03-28 13:44:44 +0300

    upd task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,27 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.print("Вызвался первый бин: ");
         System.out.println(bean.getMessage());
+
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");//отсюда писал не забыть удалить
+        System.out.print("Вызвался второй бин: ");
+        System.out.println(bean1.getMessage());
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.print("Вызвался третий бин: ");
+        System.out.println(bean2.getMeow());
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.print("Вызвался четвертый бин: ");
+        System.out.println(bean3.getMeow());
+
+        System.out.println("Результат сравнения первой пары бинов: " + (bean == bean1));
+        System.out.println("Результат сравнения второй пары бинов: " + (bean2 == bean3));
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    private String meow;
+
+    public Cat() {
+
+    }
+
+    public String getMeow() {
+        return meow;
+    }
+
+    public void setMeow(String meow) {
+        this.meow = meow;
+    }
+
+}
